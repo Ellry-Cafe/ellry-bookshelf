5c55bbcc8c0cba5ebdefac2d8dcb46f2977e2b45
elleiell <elleiellb@gmail.com>
2025-05-30 01:28:55 +0800

    PWA setup

--- src/index.js
+++ src/index.js
@@ -21,4 +21,17 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note that this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 reportWebVitals();
